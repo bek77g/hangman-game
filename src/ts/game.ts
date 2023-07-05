2dbe7a0962893e5c1bf54ecc49a7b18e03c873f2
bek77g <d.zheenkul@gmail.com>
2023-07-05 14:34:18 +0600

    add: create custom keyboard, create image of tries

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -2,8 +2,9 @@ import { WORDS } from './consts';
 import { KEYBOARD_LETTERS } from './consts';
 
 const gameDiv: HTMLDivElement = document.getElementById('game');
+const logoH1: HTMLHeadingElement = document.getElementById('logo');
 
-const createPlaceHoldersHTML = (): void => {
+const createPlaceHoldersHTML = (): string => {
   const word = sessionStorage.getItem('word');
   const wordArray = word.split('');
   const placeholdersHTML: string = wordArray.reduce((acc, _, idx) => {
@@ -13,10 +14,43 @@ const createPlaceHoldersHTML = (): void => {
   return `<div id="placeholders" class="placeholder-wrapper">${placeholdersHTML}</div>`;
 };
 
+const createKeyboard = (): HTMLDivElement => {
+  const keyboard: HTMLDivElement = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  keyboard.id = 'keyboard';
+
+  const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, curr, idx) => {
+    return (
+      acc +
+      `<button class="button-primary keyboard-button" id="${curr}">${curr}</button>`
+    );
+  }, '');
+
+  keyboard.innerHTML = keyboardHTML;
+  return keyboard;
+};
+
+const createHangManImg = (): HTMLImageElement => {
+  const image: HTMLImageElement = document.createElement('img');
+  image.src = 'images/hg-0.png';
+  image.alt = 'hangman image';
+  image.classList.add('hangman-img');
+  image.id = 'hangman-img';
+
+  return image;
+};
+
 export const startGame = (): void => {
+  logoH1.classList.add('logo-sm');
   const randomIndex: number = Math.floor(Math.random() * WORDS.length);
   const wordToGuess: string = WORDS[randomIndex];
   sessionStorage.setItem('word', wordToGuess);
 
   gameDiv.innerHTML = createPlaceHoldersHTML();
+  gameDiv.innerHTML += `<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">10</span></p>`;
+
+  const hangmanImg = createHangManImg();
+  gameDiv.prepend(hangmanImg);
+
+  gameDiv.appendChild(createKeyboard());
 };
