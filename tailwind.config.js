0d2c85d64f98bd6c31d5cf72e7fcfafe57cab38f
bek77g <d.zheenkul@gmail.com>
2023-07-05 13:52:42 +0600

    fix: js scanning to ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.js'],
+  content: ['./index.html', './src/**/*.ts'],
   theme: {
     extend: {},
   },
